82956edf4f4f1bfebd3007b4545fdd0486c001a8
dev <shagen@pop-os.localdomain>
2024-04-22 11:42:17 +0500

    connection

--- connection.php
+++ connection.php
@@ -1,11 +1,14 @@
 <?php
 echo "Hello";
-    $database= new mysqli("216.250.11.90","wordpress","{superSecretPassword!123}","edoc");
-    echo "World";
-    if ($database->connect_error){
-        echo "error";
-       die("Connection failed:  ".$database->connect_error);
-    }
-    echo "Connected successfully";
+
+$database = new mysqli("216.250.11.90", "wordpress", "{superSecretPassword!123}", "edoc");
+$database->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+$database->options(MYSQLI_INIT_COMMAND, "SET NAMES 'utf8'");
+echo "World";
+if ($database->connect_error) {
+    echo "error";
+    die("Connection failed:  " . $database->connect_error);
+}
+echo "Connected successfully";
 
 ?>
