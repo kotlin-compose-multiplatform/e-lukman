458a2c05ebe185a8dea314fcc355983a91615c98
dev <shagen@pop-os.localdomain>
2024-04-22 11:33:55 +0500

    connection fixed

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $database= new mysqli("127.0.0.1","wordpress","{superSecretPassword!123}","edoc");
+    $database= new mysqli("216.250.11.90","wordpress","{superSecretPassword!123}","edoc");
     if ($database->connect_error){
         
        die("Connection failed:  ".$database->connect_error);
