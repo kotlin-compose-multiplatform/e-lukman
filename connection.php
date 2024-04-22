8be03bcfad75417b1f7d36592e67db6b1ba6d1fd
dev <shagen@pop-os.localdomain>
2024-04-22 11:36:10 +0500

    connection

--- connection.php
+++ connection.php
@@ -1,8 +1,11 @@
 <?php
+echo "Hello";
     $database= new mysqli("216.250.11.90","wordpress","{superSecretPassword!123}","edoc");
+    echo "World";
     if ($database->connect_error){
-        
+        echo "error";
        die("Connection failed:  ".$database->connect_error);
     }
+    echo "Connected successfully";
 
 ?>
